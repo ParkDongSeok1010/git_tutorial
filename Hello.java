9879d7e386835fb8386a22696fec24d213c6878c
Park DS <jbs6871@naver.com>
2016-11-29 11:16:48 +0900

    Hello his git

--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
 public class Hello{
 	public static void main(String[] args){
 		System.out.println("Hello Git");
-		System.out.println("Hello My Hit");
+		System.out.println("Hello My Git");
+		System.out.println("Hello His Git");
 	}
 }
